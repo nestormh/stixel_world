b2b2f848f8d31281eb3d90709e1a81e1afe14d10
Néstor Morales <nestor@isaatc.ull.es>
2014-01-28 12:13:52 +0000

    Initial commit of the ROS Hydro version of the Stixels World application

---
+++ StixelWorldConfig.cmake
@@ -0,0 +1,4 @@
+# set(STIXEL_WORLD_PATH "/home/nestor/Dropbox/KULeuven/projects/StixelWorld")
+set(STIXEL_WORLD_PATH "/home/nestor/Dropbox/ros/hydro/nestormh/src/stixel_world")
+set(DENSE_TRACKER_PATH "/home/nestor/Dropbox/projects/DenseTracker")
+set(POLAR_CALIBRATION_PATH "/home/nestor/Dropbox/KULeuven/projects/PolarCalibration")
